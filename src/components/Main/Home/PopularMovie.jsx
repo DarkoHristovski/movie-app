2bcc256da58594f767821b2b345425a336259874
Darko Hristovski <hristovski15@gmail.com>
2023-12-12 00:46:59 +0100

    adding slider

--- src/components/Main/Home/PopularMovie.jsx
+++ src/components/Main/Home/PopularMovie.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 const PopularMovies = ({movies}) =>{
 
-    let mostPopularMovies = movies.filter(x=> x.fields.rating > 8);
-    
-    
-    const latestMovies = movies.slice(0, 4);
+    let mostPopularMovies = movies.sort((a,b)=> b.fields.rating-a.fields.rating).filter(x=> x.fields.rating > 7)
+    console.log(mostPopularMovies)
+
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows:true
+    };
     return (
         <>
+     
+  
         <section className="movie-wrapper">
         <div className="movie-wrapper-title">
         <h2>Most Popular Movies</h2>
         <p>Here are some of the most popular movies that our users &amp; viewers enjoy.</p>
 </div>
-<div className="flex-container">
+<div className="">
+<Slider {...settings}>
         {mostPopularMovies.map((x) => (
           <Link to={`/movies/${x.sys.id}`} className="movie-card" key={x.sys.id}>
             <img src={x.fields.image.fields.file.url} alt="" />
@@ -20,6 +33,7 @@ const PopularMovies = ({movies}) =>{
             <p className="movie-rating">Rating: {x.fields.rating}</p>
           </Link>
         ))}
+        </Slider>
       </div>
       </section>
       </>
