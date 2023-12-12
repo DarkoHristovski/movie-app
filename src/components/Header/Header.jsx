d8b3861e31f5cddb180810614026b8307e6de111
AnnaHovzan <vipcat007@gmail.com>
2023-12-12 16:14:25 +0100

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import "./header.css"
+import tvLogo from "../../icon/tv.png"
+import Home from "../Main/Home/Home"
+
 const Header = () => {
-  return <></>;
+  return (
+    <div className="header_wrapper">
+      <Link to="/" className="logo">
+          <img src={tvLogo} alt="logo" />
+          <p>MovieFriend</p>
+      </Link>
+      <div className="navLinks">
+        <Link to="/">Home</Link>
+        <Link to="/rating">Rating</Link>
+        <Link to="/release">Release</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
