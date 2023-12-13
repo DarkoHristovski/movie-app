59f0583c673091051000977b2d0e5cd066def4ed
Aadil Ahmad Chattha <136930460+adichats92@users.noreply.github.com>
2023-12-13 21:22:36 +0100

    style update of home page and cards and responsive behaviour set

--- src/components/Main/LatestMovies.jsx
+++ src/components/Main/LatestMovies.jsx
@@ -1,27 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const LatestMovies = ({movies}) =>{
+const LatestMovies = ({ movies }) => {
+	const latestMovies = movies.slice(0, 4);
+	return (
+		<>
+			<section className='movie-wrapper'>
+				<div className='movie-wrapper-title'>
+					<h2>Latest Movies</h2>
+					<p>
+						Check out the latest movies, available at our movie application.
+					</p>
+				</div>
+				<div
+					className='flex-container'
+					style={{ margin: '1rem', padding: '1rem' }}
+				>
+					{latestMovies.map((x) => (
+						<Link
+							to={`/movies/${x.sys.id}`}
+							className='movie-card'
+							key={x.sys.id}
+						>
+							<img
+								src={x.fields.image.fields.file.url}
+								alt=''
+							/>
+							<p className='movie-title'>{x.fields.title}</p>
+						</Link>
+					))}
+				</div>
+			</section>
+		</>
+	);
+};
 
-    const latestMovies = movies.slice(0, 4);
-    return (
-        <>
-        <section className="movie-wrapper">
-          <div className="movie-wrapper-title">
-        <h2>Latest Movies</h2>
-        <p>Check out the latest movies, available at our movie application.</p>
-        </div>
-<div className="flex-container">
-        {latestMovies.map((x) => (
-          <Link to={`/movies/${x.sys.id}`} className="movie-card" key={x.sys.id}>
-            <img src={x.fields.image.fields.file.url} alt="" />
-            <p className="movie-title">{x.fields.title}</p>
-          </Link>
-        ))}
-      </div>
-      </section>
-      </>
-    )
-}
-
-
-export default LatestMovies
+export default LatestMovies;
