94b84870d397261f62210b30d798913524b2af05
AnnaHovzan <vipcat007@gmail.com>
2023-12-13 23:19:42 +0100

    add style to multiple elements

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import "./Footer.css"
+import tvLogo from "../../icon/tv.png";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <div className="FooterWraper">
+        <div className="item1">
+          <Link
+            to="/"
+            className="logo"
+            style={{ textDecoration: "none", color: "#2a0a3e" }}
+          >
+            <img src={tvLogo} alt="logo" style={{ width: "50px" }} />
+          </Link>
+          <div className="contacts">
+            
+          </div>
+        </div>
+
+        <div className="item2">
+          <p>
+            Made by :
+            <a href="https://github.com/DarkoHristovski"> Darko Hristovski</a> /
+            <a href="https://github.com/AnnaHovzan">AnnaHovzan</a> /{" "}
+            <a href="https://github.com/adichats92">Aadil</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
