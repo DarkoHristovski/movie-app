c70b6e123d0c665fae359b0f260c231c193dd4ef
AnnaHovzan <vipcat007@gmail.com>
2023-12-13 13:10:39 +0100

    stable ver

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom";
-import "./header.css"
-import tvLogo from "../../icon/tv.png"
-import Home from "../Main/Home/Home"
+import "./header.css";
+import tvLogo from "../../icon/tv.png";
+import Home from "../Main/Home/Home";
+import { useState } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [isOnClick, setIsOnClick] = useState(false);
   return (
-    <div className="header_wrapper">
-      <Link to="/" className="logo">
+    <>
+      <nav className="header_wrapper">
+        <Link to="/" className="logo">
           <img src={tvLogo} alt="logo" />
           <p>MovieFriend</p>
-      </Link>
-      <div className="navLinks">
-        <Link to="/">Home</Link>
-        <Link to="/rating">Rating</Link>
-        <Link to="/release">Release</Link>
-        <Link to="/about">About</Link>
-      </div>
-    </div>
+        </Link>
+        <div className="navLinks">
+          <Link to="/">Home</Link>
+          <Link to="/rating">Rating</Link>
+          <Link to="/release">Release</Link>
+          <Link to="/about">About</Link>
+        </div>
+      </nav>
+    </>
   );
 };
 
--- src/components/Main/Home/TopSection/TopSection.jsx
+++ src/components/Main/Home/TopSection/TopSection.jsx
@@ -1,15 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
 import "./TopSectionStyle.css";
-import Slider from "react-slick";
-import TopCarousel from "./TopCarousel"
 
 const TopSection = ({ movies }) => {
   const latestMovies = movies.slice(0, 3);
-  console.log(latestMovies)
+  console.log(latestMovies);
   return (
     <>
-      <TopCarousel/>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="img"
+              src="https://www.sky.de/static/img/filmhighlights/John-Wick-Kapitel-4-Sky-23-12-Teaser2-16-9.jpg?impolicy=p_cm05"
+              alt="img"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </>
   );
 };
