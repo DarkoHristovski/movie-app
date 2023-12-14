e8ff01a5b89043c6f2d2f479cdc9efebb7ef3876
Aadil Ahmad Chattha <136930460+adichats92@users.noreply.github.com>
2023-12-14 13:22:48 +0100

    add favorite section

--- src/components/Main/Favorites/Favorites.jsx
+++ src/components/Main/Favorites/Favorites.jsx
@@ -9,20 +9,39 @@ const Favorites = () => {
 		setFavMovies(savedMovies);
 	}, []);
 
+	const removeFromFavorites = (movieId) => {
+		const updatedFavMovies = favMovies.filter(
+			(movie) => movie.sys.id !== movieId
+		);
+		localStorage.setItem('favMovies', JSON.stringify(updatedFavMovies));
+		setFavMovies(updatedFavMovies);
+	};
+
 	return (
 		<div className='movie-wrapper'>
 			<div className='flex-container'>
 				{favMovies.map((movie) => (
-					<Link
-						to={`/movies/${movie.sys.id}`}
-						className='movie-card'
-						key={movie.sys.id}
-					>
-						<img
-							src={movie.fields.image.fields.file.url}
-							alt={movie.fields.title}
-						/>
-					</Link>
+					<>
+						<div
+							className='favorite-rm-icon'
+							onClick={() => removeFromFavorites(movie.sys.id)}
+						>
+							<img
+								src='../../src/assets/remove.png'
+								alt='remove from favorites'
+							/>
+						</div>
+						<Link
+							to={`/movies/${movie.sys.id}`}
+							className='movie-card'
+							key={movie.sys.id}
+						>
+							<img
+								src={movie.fields.image.fields.file.url}
+								alt={movie.fields.title}
+							/>
+						</Link>
+					</>
 				))}
 			</div>
 		</div>
